89bc73950335a0c07131aec2eba2c7d8e1eb6acc
Hanna Murai <hannamurai@Hannas-MBP.lan1>
2021-05-11 20:12:22 -0500

    build

--- client/src/services/service.js
+++ client/src/services/service.js
@@ -2,7 +2,7 @@ import axios from "axios";
 let baseURL;
 
 process.env.NODE_ENV === "production"
-  ? (baseURL = "")
+  ? (baseURL = "https://weathe-records.herokuapp.com")
   : (baseURL = "http://localhost:5000");
 
 
